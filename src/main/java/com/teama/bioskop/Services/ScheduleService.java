71cd89c7cbb4e7886b1261c9490826f425b9e3d5
Harist Islami <islamiharist1998@gmail.com>
2022-06-21 19:37:28 +0700

    Refactor ScheduleNotFoundException into DataNotFoundException

--- src/main/java/com/teama/bioskop/Services/ScheduleService.java
+++ src/main/java/com/teama/bioskop/Services/ScheduleService.java
@@ -1,10 +1,9 @@
 package com.teama.bioskop.Services;
 
-import com.teama.bioskop.Helpers.ScheduleNotFoundException;
+import com.teama.bioskop.Helpers.DataNotFoundException;
 import com.teama.bioskop.Models.Schedule;
 import com.teama.bioskop.Repositories.ScheduleRepository;
 import lombok.AllArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -23,26 +22,26 @@ public class ScheduleService {
         return this.scheduleRepository.save(schedule);
     }
 
-    public Schedule getOneSchedule(Integer id) throws ScheduleNotFoundException {
+    public Schedule getOneSchedule(Integer id) throws DataNotFoundException {
         Optional<Schedule> optionalSchedule = this.scheduleRepository.findById(id);
 
         if(optionalSchedule.isEmpty()){
-            throw new ScheduleNotFoundException("Schedule is Not Available");
+            throw new DataNotFoundException("Schedule is Not Available");
         }
 
         return optionalSchedule.get();
     }
 
-    public Schedule updateSchedule(Schedule schedule) throws ScheduleNotFoundException {
+    public Schedule updateSchedule(Schedule schedule) throws DataNotFoundException {
         this.getOneSchedule(schedule.getScheduleId());
 
         return this.scheduleRepository.save(schedule);
     }
 
-    public void deleteSchedule(Schedule schedule) throws ScheduleNotFoundException {
+    public void deleteSchedule(Schedule schedule) throws DataNotFoundException {
         Optional<Schedule> deletedSchedule = this.scheduleRepository.findById(schedule.getScheduleId());
         if(deletedSchedule.isEmpty()){
-            throw new ScheduleNotFoundException("Schedule is Not Available");
+            throw new DataNotFoundException("Schedule is Not Available");
         }
 
         this.scheduleRepository.delete(deletedSchedule.get());
