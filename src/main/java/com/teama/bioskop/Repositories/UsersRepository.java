c64d1d61af65dccfa7d25d3959175b5a9e9956df
habibzuhdi3 <habibzuhdi3@gmail.com>
2022-06-21 14:55:43 +0700

    create seats Service and fix Repositories

--- src/main/java/com/teama/bioskop/Repositories/UsersRepository.java
+++ src/main/java/com/teama/bioskop/Repositories/UsersRepository.java
@@ -1,7 +1,10 @@
 package com.teama.bioskop.Repositories;
 
+import com.teama.bioskop.Models.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface UsersRepository extends JpaRepository<UsersRepository,Integer>{
+@Repository
+public interface UsersRepository extends JpaRepository<Users,Integer>{
 
 }
