3805cbe95c8c7735567a97655c51776a7da3e30f
anik j <juniyatianik@gmail.com>
2022-06-21 15:51:08 +0700

    membuat 4 controller by anik

---
+++ src/main/java/com/teama/bioskop/Controller/FilmsController.java
@@ -0,0 +1,21 @@
+package com.teama.bioskop.Controller;
+
+import com.teama.bioskop.Models.Films;
+import com.teama.bioskop.Services.FilmsService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@AllArgsConstructor
+public class FilmsController {
+    private final FilmsService filmsService;
+
+    @GetMapping("/films")
+    public List<Films> getAll() {
+        return this.filmsService.getAllFilms();
+
+    }
+}
