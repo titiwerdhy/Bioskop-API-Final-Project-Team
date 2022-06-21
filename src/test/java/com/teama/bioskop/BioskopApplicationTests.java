8d6c100369fd34d1be706e76be907048a66ab212
najibdj27 <najib.djulfikar@gmail.com>
2022-06-21 11:05:28 +0700

    Initiate spring boot project

---
+++ src/test/java/com/teama/bioskop/BioskopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teama.bioskop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BioskopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
