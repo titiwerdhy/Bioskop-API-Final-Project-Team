9f94802148c27f99744bf700b211c07d0098ab34
Rifky Hamdhani <rifky.dani@gmail.com>
2022-06-21 14:59:59 +0700

    commit

--- src/main/java/com/teama/bioskop/Repositories/FilmsRepository.java
+++ src/main/java/com/teama/bioskop/Repositories/FilmsRepository.java
@@ -1,7 +1,11 @@
 package com.teama.bioskop.Repositories;
 
+import com.teama.bioskop.Models.Films;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface Films extends JpaRepository<Films,Integer>{
+@Repository
+
+public interface FilmsRepository extends JpaRepository<Films,Integer>{
 
 }
