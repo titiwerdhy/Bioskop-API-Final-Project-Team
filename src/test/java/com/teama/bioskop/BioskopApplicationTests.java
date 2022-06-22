0885a95bfca42a76c3fbb3767c53267b1a053b3c
najibdj27 <najib.djulfikar@gmail.com>
2022-06-22 10:31:24 +0700

    Delete test

--- src/test/java/com/teama/bioskop/BioskopApplicationTests.java
+++ src/test/java/com/teama/bioskop/BioskopApplicationTests.java
@@ -1,13 +1,10 @@
 package com.teama.bioskop;
 
-import org.junit.jupiter.api.Test;
+// import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class BioskopApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
 
 }
