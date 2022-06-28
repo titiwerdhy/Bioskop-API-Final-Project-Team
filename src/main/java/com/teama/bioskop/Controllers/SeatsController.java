14d1d77e366de0288e2acdc8d36203114c1c2cfe
Rifky Hamdhani <rifky.dani@gmail.com>
2022-06-28 10:44:08 +0700

    update loger seat

--- src/main/java/com/teama/bioskop/Controllers/SeatsController.java
+++ src/main/java/com/teama/bioskop/Controllers/SeatsController.java
@@ -6,6 +6,8 @@ import com.teama.bioskop.Helpers.DataNotFoundException;
 import com.teama.bioskop.Models.Seats;
 import com.teama.bioskop.Services.SeatsService;
 import lombok.AllArgsConstructor;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,11 +17,14 @@ import java.util.List;
 @RestController
 @AllArgsConstructor
 public class SeatsController {
+
+    private static final Logger logger = LogManager.getLogger(SeatsController.class);
     private final SeatsService seatsService;
 
     @GetMapping("/seats")
     public ResponseEntity<List<Seats>> getAll() {
         List<Seats> allSeats = this.seatsService.getAllSeats();
+        logger.info("Get all seat: " +allSeats);
         return ResponseEntity.status(HttpStatus.OK).body(allSeats);
     }
 
@@ -27,18 +32,21 @@ public class SeatsController {
     public ResponseEntity<SeatsResponseDTO> getById(@PathVariable("id") Integer id) throws DataNotFoundException {
         Seats seats = this.seatsService.getSeatById(id);
         SeatsResponseDTO seatsResponseDTO = seats.convertToResponse();
+        logger.info("Get seat by id : " + seats);
         return ResponseEntity.status(HttpStatus.OK).body(seatsResponseDTO);
     }
 
     @PostMapping("/seats")
     public ResponseEntity<Seats> InsertSeats(@RequestBody SeatsRequestDTO seatsRequestDTO) {
         Seats createdSeat = seatsService.insertNewSeats(seatsRequestDTO.converToSeat());
+        logger.info("Create new seat : " + createdSeat );
         return ResponseEntity.status(HttpStatus.OK).body(createdSeat);
     }
 
     @PutMapping("/seats")
     public ResponseEntity<Seats> UpdateSeats(@RequestBody SeatsRequestDTO seatsRequestDTO) {
         Seats updatedSeat = seatsService.UpdateSeats(seatsRequestDTO.converToSeat());
+        logger.info("Update new seat : " + updatedSeat);
         return ResponseEntity.status(HttpStatus.OK).body(updatedSeat);
     }
 
@@ -46,12 +54,14 @@ public class SeatsController {
     public ResponseEntity<Seats> deleteSeat(@PathVariable("id") Integer id) throws DataNotFoundException {
         Seats deletedSeat = seatsService.getSeatById(id);
         seatsService.deleteSeatById(id);
+        logger.info("Delete seat : " + deletedSeat);
         return ResponseEntity.status(HttpStatus.OK).body(deletedSeat);
     }
 
     @PostMapping("/seats/available")
     public ResponseEntity<List<Seats>> findSeatsAvailable(@RequestBody Seats seats){
         List<Seats> seatsAvailable = this.seatsService.getSeatsAvailable(seats.getIsAvailable());
+        logger.info("Create new seat available : " + seatsAvailable);
         return ResponseEntity.status(HttpStatus.OK).body(seatsAvailable);
     }
 }
