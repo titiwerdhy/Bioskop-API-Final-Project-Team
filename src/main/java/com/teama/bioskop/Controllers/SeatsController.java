4a356e14d954c5847f91cb565571b6378dd18771
billymarcelino058 <billymarcelino058@gmail.com>
2022-07-05 11:21:22 +0700

    update controller seats mvc

---
+++ src/main/java/com/teama/bioskop/Controllers/SeatsController.java
@@ -0,0 +1,75 @@
+package com.teama.bioskop.Controllers;
+
+import com.teama.bioskop.Helpers.DataNotFoundException;
+import com.teama.bioskop.Models.Seats;
+import com.teama.bioskop.Services.SeatsService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@AllArgsConstructor
+public class SeatsController {
+
+    private final SeatsService seatsService;
+
+    /***
+     * Crud Seats
+     * @param model
+     * @return
+     */
+    @GetMapping("/crud/seats")
+    public String getAll(Model model){
+        List<Seats> seatsList = this.seatsService.getAllSeats();
+        model.addAttribute("seats", seatsList);
+        model.addAttribute("newSeats", new Seats());
+        model.addAttribute("updateSeats", new Seats());
+        return "seats-crud";
+    }
+
+    /***
+     * Create Seats
+     * @param model
+     * @param seats
+     * @return
+     */
+    @PostMapping("/create/seat")
+    public String createNew(Model model, @ModelAttribute Seats seats){
+        seatsService.insertNewSeats(seats);
+        return "redirect:/crud/seats";
+    }
+
+    /***
+     * Update Seats
+     * @param model
+     * @param id
+     * @param seats
+     * @return
+     */
+    @PutMapping("/update/seat/{id}")
+    public String updateById(Model model, @PathVariable("id") Integer id, @ModelAttribute Seats seats) {
+        seats.setSeatId(id);
+        seatsService.UpdateSeats(seats);
+        return "redirect:/crud/seats";
+    }
+
+    /***
+     * Delete Seats
+     * @param model
+     * @param id
+     * @return
+     * @throws DataNotFoundException
+     */
+    @DeleteMapping("/delete/seat/{id}")
+    public String deleteById(Model model, @PathVariable("id") Integer id) throws DataNotFoundException {
+        seatsService.deleteSeatById(id);
+        return "redirect:/crud/seats";
+    }
+
+
+
+
+}
