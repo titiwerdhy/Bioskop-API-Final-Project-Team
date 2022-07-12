7ff09fd5402c23b8bf312d3d3a7d6be9c29e6a0e
aris kusnul widitama <ariskusnul@gmail.com>
2022-07-12 10:57:14 +0700

    add pagination film

--- src/main/java/com/teama/bioskop/Services/FilmsService.java
+++ src/main/java/com/teama/bioskop/Services/FilmsService.java
@@ -5,6 +5,9 @@ import com.teama.bioskop.Models.Films;
 import com.teama.bioskop.Repositories.FilmsRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 import java.util.Optional;
@@ -72,5 +75,10 @@ public class FilmsService {
         }
 
         return filmsList;
+    }
+
+    public Page<Films> getAllFilmsPaged(int pageNo, int pageSize) {
+        Pageable pageable = PageRequest.of(pageNo - 1, pageSize);
+        return this.filmsRepository.findAll(pageable);
     }
 }
